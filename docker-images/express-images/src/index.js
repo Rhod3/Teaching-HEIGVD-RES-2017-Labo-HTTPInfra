66d8b964c72d804a92d9afa39e5e364cd963dde0
Nicolas Rod <ncls.rod@gmail.com>
2017-05-23 11:27:08 +0200

    exemple génération de students aléatoire

--- docker-images/express-images/src/index.js
+++ docker-images/express-images/src/index.js
@@ -9,9 +9,38 @@ app.get('/test', function(req, res){
 });
 
 app.get('/', function(req, res){
-    res.send("Hello RES");
+    res.send(generateStudents());
 });
 
 app.listen(3000, function () {
     console.log('Accepting http request on port 3000!');
 });
+
+
+function generateStudents() {
+    var numberOfStudents = chance.integer({
+        min: 0,
+        max: 10
+    });
+    console.log(numberOfStudents);
+    var students = [];
+    for (var i = 0; i < numberOfStudents; i++) {
+        var gender = chance.gender();
+        var birthYear = chance.year({
+            min: 1986,
+            max: 1996
+        });
+        students.push({
+            firstname: chance.first({
+                gender: gender
+            }),
+            lastName: chance.last(),
+            gender: gender,
+            birthday : chance.birthday({
+                year: birthYear
+            })
+        });
+    };
+    console.log(students);
+    return students;
+}
