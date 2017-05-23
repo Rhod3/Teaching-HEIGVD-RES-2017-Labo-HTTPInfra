2325c61f2d80295fd9173feb0bbc1f21d8d0ab28
Nicolas Rod <ncls.rod@gmail.com>
2017-05-23 11:08:41 +0200

    express app example

--- docker-images/express-images/src/index.js
+++ docker-images/express-images/src/index.js
@@ -1,4 +1,17 @@
 var Chance = require('chance');
 var chance = new Chance();
 
-console.log("Coucou " + chance.name());
+var express = require("express");
+var app = express();
+
+app.get('/test', function(req, res){
+    res.send("Hello RES - accessing /test");
+});
+
+app.get('/', function(req, res){
+    res.send("Hello RES");
+});
+
+app.listen(3000, function () {
+    console.log('Accepting http request on port 3000!');
+});
