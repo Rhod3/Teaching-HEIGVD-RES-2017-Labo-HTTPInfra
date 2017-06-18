81d193ffe2e938bb51c070747c3d4cd70149828d
Nicolas Rod <ncls.rod@gmail.com>
2017-06-18 17:51:57 +0200

    demo_step3.sh

---
+++ demo_step3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+docker kill $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+
+docker rmi res/apache_static
+docker rmi res/express_dynamic
+docker rmi res/apache_rp
+
+docker build -t res/apache_static docker-images/apache-php-images/.
+docker build -t res/express_dynamic docker-images/express-images/.
+docker build -t res/apache_rp docker-images/apache-reverse-proxy/.
+
+docker run -d res/apache_static
+docker run -d res/express_dynamic
+docker run -d -p 8080:80 res/apache_rp
+
+# Access http://demo.res.ch:8080/api/students/
+# Access http://demo.res.ch:8080/
